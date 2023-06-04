ef36fe0b48c94c205a4c0457e1f7f0a3b6cb1170
malalin1 <92686066+malalin1@users.noreply.github.com>
2023-06-04 09:46:25 +0200

    deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course :-) - Mit eigenem Repositorie verbunden und Text ergänzt');
+    res.send('Hello DevOps Course :-) - Deployment');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
